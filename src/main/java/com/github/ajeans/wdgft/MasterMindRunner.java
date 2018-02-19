a659c2c38dd1a9cd3f2b4064e0562064dbbf2c27
Arnaud Jeansen <arnaud.jeansen@gmail.com>
2018-02-19 20:18:17 +0100

    Lay out the work in main

--- src/main/java/com/github/ajeans/wdgft/MasterMindRunner.java
+++ src/main/java/com/github/ajeans/wdgft/MasterMindRunner.java
@@ -1,10 +1,15 @@
 package com.github.ajeans.wdgft;
 
+import com.github.ajeans.wdgft.model.Combination;
+
 public class MasterMindRunner {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        // TODO computer picks combination randomly
+        Combination computer = new Combination("RYBO");
+        // TODO request input from user
+        // TODO check request against our combination
+        // TODO output result
     }
 
 }
